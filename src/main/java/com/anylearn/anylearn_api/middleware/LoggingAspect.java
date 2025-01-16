e8eb74eaa6fe01d1d52d37f8cfbb3c0eaca245c8
CoT <iamcot@gmail.com>
2025-01-16 15:15:14 +0700

    Add logging aspect for method execution tracking and set logging level to DEBUG

---
+++ src/main/java/com/anylearn/anylearn_api/middleware/LoggingAspect.java
@@ -0,0 +1,30 @@
+package com.anylearn.anylearn_api.middleware;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Around("execution(* com.anylearn..*.*(..))")
+    public Object logMethodExecution(ProceedingJoinPoint joinPoint) 
+    throws Throwable {
+        long startTime = System.currentTimeMillis();
+        logger.debug("---Entering Method: {}", joinPoint.getSignature());
+
+        Object resuObject;
+        try {
+            resuObject = joinPoint.proceed();
+        } finally {
+            long endTime = System.currentTimeMillis();
+            logger.debug("Exitting method {} (Time taken: {}ms)", joinPoint.getSignature(), endTime - startTime);
+        }
+        return resuObject;
+    }
+}
